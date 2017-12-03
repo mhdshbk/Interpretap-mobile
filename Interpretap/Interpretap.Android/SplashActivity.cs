f7eaff6e45da8c2905833908d52afaa6c2003e4f
harisbotic <haris.botic96@gmail.com>
2017-12-03 20:52:51 +0100

    [Android] Add Splash Screen

---
+++ Interpretap/Interpretap.Android/SplashActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Interpretap.Droid
+{
+    [Activity(Label = "Interpretap", Icon = "@drawable/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            await Task.Delay(2000); // 2 Seconds
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        // Make Back Button do nothing
+        public override void OnBackPressed() { }
+    }
+}
