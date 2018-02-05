28b7b256e114bdb8d279ca62ebf31db567e2658a
Daniil Mikhin <damihin@gmail.com>
2018-02-05 14:10:29 +0700

    Fix keyboard overlapping an entry

--- Interpretap/Interpretap.iOS/AppDelegate.cs
+++ Interpretap/Interpretap.iOS/AppDelegate.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using KeyboardOverlap.Forms.Plugin.iOSUnified;
 using UIKit;
 
 namespace Interpretap.iOS
@@ -23,6 +24,7 @@ namespace Interpretap.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            KeyboardOverlapRenderer.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
